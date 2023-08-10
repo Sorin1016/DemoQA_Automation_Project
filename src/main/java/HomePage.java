2d821068138d13718823ded0b2023fa06870c8cf
Sorin <sorinhorcea@gmail.com>
2023-08-10 21:02:24 +0300

    Added BooksTest

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -2,6 +2,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class HomePage extends BasePage {
 
     public HomePage(WebDriver driver) {
@@ -11,11 +13,23 @@ public class HomePage extends BasePage {
     @FindBy(xpath = "//h5[contains(text(),'Book Store Application')]")
     private WebElement bookStoreAplicationButton;
 
+    @FindBy(css = ".rt-tr-group")
+    List<WebElement> booklist;
+
     public void bookStoreClick() {
         this.bookStoreAplicationButton.click();
     }
 
     public void scrollDown() {
-       scrollToElement(this.bookStoreAplicationButton);
+        scrollToElement(this.bookStoreAplicationButton);
+    }
+
+    public void printAllBooks() {
+        for (WebElement element : booklist) {
+            if (!element.getText().isBlank()) {
+                System.out.println(element.getText());
+                System.out.println("------------------------------------");
+            }
+        }
     }
 }
